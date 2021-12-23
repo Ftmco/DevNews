619b1509919b8a95ebc02112698f637073dff236
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-23 06:41:48 -0800

    chagne project

---
+++ DevNews/DevNews.Server/Article.Web.Server/app.ts
@@ -0,0 +1,58 @@
+import * as express from 'express';
+import { AddressInfo } from "net";
+import * as path from 'path';
+import * as bodyParser from 'body-parser'
+
+
+const app = express();
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json({ limit: '100mb' }))
+app.use(express.urlencoded({ limit: '100mb' }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }))
+app.use(bodyParser.json({ limit: '100mb' }))
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err['status'] = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err, req, res, next) => { // eslint-disable-line @typescript-eslint/no-unused-vars
+        res.status(err['status'] || 500);
+        res.json({
+            status: false,
+            code: 500
+        })
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => { // eslint-disable-line @typescript-eslint/no-unused-vars
+    res.status(err.status || 500);
+    res.json({
+        status: false,
+        code: err.status,
+        message: err.message
+    })
+});
+
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), function () {
+
+    console.log(`Application Started On ${(server.address() as AddressInfo).port}`)
+});
