2179dea00e1e870b06c7727799d759fd5ab64b9c
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-31 04:15:36 -0800

    update

--- DevNews/DevNews.Client/.nuxt/components/index.js
+++ DevNews/DevNews.Client/.nuxt/components/index.js
@@ -1,6 +1,9 @@
 export { default as CoreAppBar } from '../..\\components\\core\\AppBar.vue'
 export { default as CoreBottomNavigation } from '../..\\components\\core\\BottomNavigation.vue'
 export { default as CoreNavigationDrawer } from '../..\\components\\core\\NavigationDrawer.vue'
+export { default as CoreTheLoading } from '../..\\components\\core\\TheLoading.vue'
+export { default as CoreTheSnackbar } from '../..\\components\\core\\TheSnackbar.vue'
+export { default as AccountBase } from '../..\\components\\account\\AccountBase.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
