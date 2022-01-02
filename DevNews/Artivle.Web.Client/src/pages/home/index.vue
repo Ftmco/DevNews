1a690364d3354b7fb35ee50d683bde89a68a80c2
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-02 12:20:19 -0800

    Update index.vue

--- DevNews/Artivle.Web.Client/src/pages/home/index.vue
+++ DevNews/Artivle.Web.Client/src/pages/home/index.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>
-       
-    </div>
+  <div>
+    <v-card>
+      <ChannelTemprory />
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
+import ChannelTemprory from "@/components/channel/ChannelTemprory.vue";
+import Vue from "vue";
 
+export default Vue.extend({
+  components: { ChannelTemprory },
+});
 </script>
