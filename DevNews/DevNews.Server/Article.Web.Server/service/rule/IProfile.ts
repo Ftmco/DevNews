0f57ff35842f5aa18db5d4e318ad2bd25b1f7c9b
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-02 12:20:06 -0800

    Update IProfile.ts

--- DevNews/DevNews.Server/Article.Web.Server/service/rule/IProfile.ts
+++ DevNews/DevNews.Server/Article.Web.Server/service/rule/IProfile.ts
@@ -1,6 +1,9 @@
 import { IncomingHttpHeaders } from "http2";
+import { ProfileModel } from "../../model/account";
 
 
 export default interface IProfile {
     get(headers: IncomingHttpHeaders): any;
+
+    update(header: IncomingHttpHeaders, profile: ProfileModel): any;
 }
