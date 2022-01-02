1ab9c7aef0bb0fe753c3c1ba8a3b2032381fb282
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-02 12:21:45 -0800

    Create channel.ts

---
+++ DevNews/DevNews.Server/Article.Web.Server/model/channel.ts
@@ -0,0 +1,12 @@
+export type ChannelModel = {
+    link: string;
+    avatar: string[];
+    name: string;
+    token: string;
+    id: string;
+}
+
+export type SaveFile = {
+    base64: string;
+    path: string;
+}
