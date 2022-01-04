54d1f860fc48299c8e58acfc56c9ee220378cdbb
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-04 10:48:57 -0800

    Update homeLayout.vue

--- DevNews/Artivle.Web.Client/src/layouts/homeLayout.vue
+++ DevNews/Artivle.Web.Client/src/layouts/homeLayout.vue
@@ -1,33 +1,32 @@
 <template>
-    <div>
-        <AppBar />
-        
-        <v-main>
-            <router-view />
-        </v-main>
-        <br/><br/>
-        <TheBottomNavigation />
-        <TheSnackbar />
-        <TheLoading />
-    </div>
+  <div>
+    <AppBar />
+
+    <v-main>
+      <router-view />
+    </v-main>
+    <br /><br />
+    <TheBottomNavigation />
+    <TheSnackbar />
+    <TheLoading />
+  </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import AppBar from '@/components/core/AppBar.vue'
-    import TheBottomNavigation from '@/components/core/TheBottomNavigation.vue'
-    import TheSnackbar from "@/components/core/TheSnackbar.vue"
-    import TheLoading from '@/components/core/TheLoading.vue'
+import Vue from "vue";
+import AppBar from "@/components/core/AppBar.vue";
+import TheBottomNavigation from "@/components/core/TheBottomNavigation.vue";
+import TheSnackbar from "@/components/core/TheSnackbar.vue";
+import TheLoading from "@/components/core/TheLoading.vue";
 
-    export default Vue.extend({
-
-        components: {
-            AppBar,
-            TheBottomNavigation,
-            TheSnackbar,
-            TheLoading
-        },
-    })
+export default Vue.extend({
+  components: {
+    AppBar,
+    TheBottomNavigation,
+    TheSnackbar,
+    TheLoading,
+  },
+});
 </script>
 
 <style scoped>
