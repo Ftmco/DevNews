8d82735d087496f2d1c5450faa9ccac66e1e80bb
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-04 10:48:37 -0800

    create channel service

--- DevNews/Artivle.Web.Client/src/api/rules/channel.rule.ts
+++ DevNews/Artivle.Web.Client/src/api/rules/channel.rule.ts
@@ -1,3 +1,8 @@
+import { Channel } from "../models/channel.model"
+
 export default interface IChannelRule {
     getChannels(): any
+
+    creatChannel(channel:Channel):any
+
 }
--- DevNews/Artivle.Web.Client/src/api/service/channel.service.ts
+++ DevNews/Artivle.Web.Client/src/api/service/channel.service.ts
@@ -1,5 +1,6 @@
 import { messages } from "@/constants";
 import { AxiosInstance } from "axios";
+import { Channel } from "../models/channel.model";
 import IChannelRule from "../rules/channel.rule";
 
 export default class ChannelService implements IChannelRule {
@@ -10,11 +11,21 @@ export default class ChannelService implements IChannelRule {
         this._axios = axios;
     }
 
+    async creatChannel(channel: Channel) {
+        try {
+            let request = await this._axios.post("Channel/Create", channel)
+            return await request.data
+        }
+        catch (e: any) {
+            return messages.netWorkError(e.message)
+        }
+    }
+
     async getChannels() {
         try {
             let request = await this._axios.get("Channels/Get")
             return await request.data
-        } catch (e) {
+        } catch (e: any) {
             return messages.netWorkError(e.message)
         }
     }
