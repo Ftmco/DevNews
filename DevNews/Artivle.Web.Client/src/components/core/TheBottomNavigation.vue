bcde7ae47fcfc09360ec6373ce3f2774e055d2d4
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-06 00:56:30 -0800

    Update TheBottomNavigation.vue

--- DevNews/Artivle.Web.Client/src/components/core/TheBottomNavigation.vue
+++ DevNews/Artivle.Web.Client/src/components/core/TheBottomNavigation.vue
@@ -1,33 +1,42 @@
 <template>
-    <div v-if="show">
-        <v-bottom-navigation v-model="page"
-                             shift
-                             fixed
-                             grow
-                             color="primary">
-            <v-btn v-for="item in items" :key="item.id" :to="item.to">
-                <span>{{item.title}}</span>
+  <div v-if="show">
+    <v-bottom-navigation v-model="page" shift fixed grow color="primary">
+      <v-btn v-for="item in items" :key="item.id" :to="item.to">
+        <span>{{ item.title }}</span>
 
-                <v-icon>{{item.icon}}</v-icon>
-            </v-btn>
-        </v-bottom-navigation>
-    </div>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import { bottomItems } from '@/constants/'
+import Vue from "vue";
+import { bottomItems } from "@/constants/";
 
-    export default Vue.extend({
-        data: () => ({
-            page: 0,
-            show: false,
-            items: bottomItems
-        }),
-        mounted() {
-            this.show = window.screen.width <= 700
-        }
-    })
+export default Vue.extend({
+  data: () => ({
+    page: 0,
+    show: false,
+    items: bottomItems,
+  }),
+  watch: {
+    $route(route) {
+      let meta = route.meta(route);
+      if (meta.noBottomNavigation) {
+        this.show = false;
+      } else this.setShow();
+    },
+  },
+  mounted() {
+    this.setShow();
+  },
+  methods: {
+    setShow() {
+      this.show = window.screen.width <= 700;
+    },
+  },
+});
 </script>
 
 <style scoped>
