06fde8546bf80bad2261964808aff0dad86e3963
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-14 10:06:25 -0800

    Create AppSetting.cs

---
+++ DevNews/Tools/AppSetting/AppSetting.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Tools.AppSetting;
+
+public static class AppSetting
+{
+    public static async Task<string> GetDataAsync(this string obj, string key)
+        => await Task.Run(async () =>
+        {
+            string? file = await File.ReadAllTextAsync(Directory.GetCurrentDirectory() + "/appsettings.json");
+            JObject? json = JObject.Parse(file);
+            return json[obj][key].ToString();
+        });
+}
