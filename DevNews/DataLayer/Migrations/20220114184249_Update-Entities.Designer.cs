b11eacff410a7122ca3e379a3f7b668ccdafb64d
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-14 10:43:16 -0800

    update data base

--- DevNews/DataLayer/Migrations/20220114184249_Update-Entities.Designer.cs
+++ DevNews/DataLayer/Migrations/20220114184249_Update-Entities.Designer.cs
@@ -4,6 +4,7 @@ using DataLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataLayer.Migrations
 {
     [DbContext(typeof(ArticleContext))]
-    partial class ArticleContextModelSnapshot : ModelSnapshot
+    [Migration("20220114184249_Update-Entities")]
+    partial class UpdateEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DevNews/DataLayer/Migrations/20220114184249_Update-Entities.cs
@@ -0,0 +1,466 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    public partial class UpdateEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ActiveCode",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Session",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "Session",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Post",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Post",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<short>(
+                name: "OwnerType",
+                table: "Post",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrginalName",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Directory",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Channel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Article",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<short>(
+                name: "OwnerType",
+                table: "Article",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShortDescription",
+                table: "Article",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Article",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "Article",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Article",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Session_UserId",
+                table: "Session",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChannelsUsers_ChannelId",
+                table: "ChannelsUsers",
+                column: "ChannelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChannelsUsers_UserId",
+                table: "ChannelsUsers",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChannelsAdmins_ChannelId",
+                table: "ChannelsAdmins",
+                column: "ChannelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChannelsAdmins_UserId",
+                table: "ChannelsAdmins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleCategories_ArticleId",
+                table: "ArticleCategories",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleCategories_CategoryId",
+                table: "ArticleCategories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ArticleCategories_Article_ArticleId",
+                table: "ArticleCategories",
+                column: "ArticleId",
+                principalTable: "Article",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ArticleCategories_Category_CategoryId",
+                table: "ArticleCategories",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChannelsAdmins_Channel_ChannelId",
+                table: "ChannelsAdmins",
+                column: "ChannelId",
+                principalTable: "Channel",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChannelsAdmins_User_UserId",
+                table: "ChannelsAdmins",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChannelsUsers_Channel_ChannelId",
+                table: "ChannelsUsers",
+                column: "ChannelId",
+                principalTable: "Channel",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChannelsUsers_User_UserId",
+                table: "ChannelsUsers",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Session_User_UserId",
+                table: "Session",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ArticleCategories_Article_ArticleId",
+                table: "ArticleCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ArticleCategories_Category_CategoryId",
+                table: "ArticleCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChannelsAdmins_Channel_ChannelId",
+                table: "ChannelsAdmins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChannelsAdmins_User_UserId",
+                table: "ChannelsAdmins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChannelsUsers_Channel_ChannelId",
+                table: "ChannelsUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChannelsUsers_User_UserId",
+                table: "ChannelsUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Session_User_UserId",
+                table: "Session");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Session_UserId",
+                table: "Session");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ChannelsUsers_ChannelId",
+                table: "ChannelsUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ChannelsUsers_UserId",
+                table: "ChannelsUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ChannelsAdmins_ChannelId",
+                table: "ChannelsAdmins");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ChannelsAdmins_UserId",
+                table: "ChannelsAdmins");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ArticleCategories_ArticleId",
+                table: "ArticleCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ArticleCategories_CategoryId",
+                table: "ArticleCategories");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Post");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerType",
+                table: "Post");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Article");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerType",
+                table: "Article");
+
+            migrationBuilder.DropColumn(
+                name: "ShortDescription",
+                table: "Article");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Article");
+
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "Article");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Article");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ActiveCode",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "Session",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "Session",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Post",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrginalName",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Directory",
+                table: "File",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Channel",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Category",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
