cd15ae34970ca8333e7597d7e74b4fa4e49cf82b
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-14 09:50:00 -0800

    create base servicces

---
+++ DevNews/Service/Base/IBaseRules.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+namespace Services.Base;
+
+internal interface IBaseRules<Tentity> where Tentity : class
+{
+    Task<IEnumerable<Tentity>> GetAsync();
+
+    Task<IEnumerable<Tentity>> GetAsync(Expression<Func<Tentity, bool>> where);
+
+    Task<Tentity> GetAsync(object id);
+
+    Task<bool> InsertAsync(Tentity entity);
+
+    Task<bool> InsertAsync(IEnumerable<Tentity> tentities);
+
+    Task<bool> UpdateAsync(Tentity entity);
+
+    Task<bool> UpdateAsync(IEnumerable<Tentity> tentities);
+
+    Task<bool> DeleteAsync(Tentity entity);
+
+    Task<bool> DeleteAsync(IEnumerable<Tentity> tentities);
+
+    Task<bool> DeleteAsync(object id);
+
+    Task<bool> DeleteAsync(Expression<Func<Tentity, bool>> where);
+
+    Task<bool> SaveAsync();
+}
+
