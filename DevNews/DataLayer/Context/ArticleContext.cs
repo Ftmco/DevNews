1481529b05fb8dc062e5f79ade017d1f8a938cec
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-14 09:12:43 -0800

    create layers

---
+++ DevNews/DataLayer/Context/ArticleContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Context;
+
+public class ArticleContext : DbContext
+{
+    public ArticleContext(DbContextOptions<ArticleContext> options):base(options)
+    {
+
+    }
+
+
+}
