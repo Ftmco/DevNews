393a0f41f4d1d6606ea749a8f09b335ff44c3a46
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-16 10:58:13 -0800

    Create Code.cs

---
+++ DevNews/Tools/Code/Code.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Code;
+
+public static class CodeGenerator
+{
+    public static string GenerateNumberCode(this int count)
+        => Guid.NewGuid().GetHashCode().ToString()[..count];
+}
