26a5e5ac561a486beeea4bb5ef9106950b20c735
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-16 11:09:53 -0800

    Update Email.cs

--- DevNews/Tools/Email/Email.cs
+++ DevNews/Tools/Email/Email.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tools.Email;
 
@@ -20,6 +15,7 @@ public static class EmailExtensions
                 IsBodyHtml = true,
                 Body = body,
             };
+            mailMessage.To.Add(email);
             SmtpClient smtpClient = new()
             {
                 Port = 587,
