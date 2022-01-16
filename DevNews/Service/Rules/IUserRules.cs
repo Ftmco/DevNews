5cc9ad50c4cf5ec4fc9e4d4be7b492d4bd584e80
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-16 10:58:45 -0800

    Create User Services

---
+++ DevNews/Service/Rules/IUserRules.cs
@@ -0,0 +1,17 @@
+﻿using Entity.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel.Account;
+
+namespace Service.Rules;
+
+public interface IUserRules : IDisposable
+{
+    Task<User> GetUserByUserNameAsync(string userName);
+
+    Task<User> FindUserAsync(string userName,string password);
+    Task<User> CreateUserAsync(SignUpViewModel signUp);
+}
---
+++ DevNews/Service/Service/UserServices.cs
@@ -0,0 +1,59 @@
+﻿using Entity.User;
+using Service.Rules;
+using Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools.Code;
+using Tools.Crypto;
+using ViewModel.Account;
+
+namespace Service.Service;
+
+public class UserServices : IUserRules
+{
+    private readonly IBaseRules<User> _userCrud;
+
+    public UserServices(IBaseRules<User> userCrud)
+    {
+        _userCrud = userCrud;
+    }
+
+    public async Task<User> CreateUserAsync(SignUpViewModel signUp)
+        => await Task.Run(async () =>
+        {
+            User newUser = new()
+            {
+                ActiveCode = 5.GenerateNumberCode(),
+                Email = signUp.Email,
+                IsActiive = false,
+                MobileNo = signUp.MobileNo,
+                Password = signUp.Password.CreateSHA256(),
+                RegisterDate = DateTime.Now,
+                UserName = signUp.UserName
+            };
+            return await _userCrud.InsertAsync(newUser) ?
+               newUser : null;
+        });
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+
+    public async Task<User> FindUserAsync(string userName, string password)
+        => await Task.Run(async () =>
+        {
+            User user = await GetUserByUserNameAsync(userName);
+            return user != null
+                ? user.Password == password.CreateSHA256() ?
+                    user : null
+                : null;
+        });
+
+    public async Task<User> GetUserByUserNameAsync(string userName)
+        => await Task.FromResult(await _userCrud.FirstOrDefaultAsync(u =>
+         u.UserName == userName || u.MobileNo == userName || u.Email == userName));
+}
