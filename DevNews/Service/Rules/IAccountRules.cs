fcd3ecc0be8958d1f2b540756d7e6703282064d2
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-16 10:58:06 -0800

    Account Services

--- DevNews/Service/Rules/IAccountRules.cs
+++ DevNews/Service/Rules/IAccountRules.cs
@@ -1,6 +1,12 @@
-﻿namespace Service.Rules;
+﻿using ViewModel.Account;
+
+namespace Service.Rules;
 
 public interface IAccountRules : IDisposable
 {
+    Task<LoginResponse> LoginAsync(LoginViewModel login);
 
+    Task<SignUpStatus> SignUpAsync(SignUpViewModel signUp);
+
+    Task<ActivationStatus> ActivationAsync(ActivationViewModel activation);
 }
--- DevNews/Service/Service/AccountServices.cs
+++ DevNews/Service/Service/AccountServices.cs
@@ -1,11 +1,79 @@
-﻿using Service.Rules;
+﻿using Entity.User;
+using Service.Rules;
+using Services.Base;
+using Tools.Code;
+using ViewModel.Account;
 
 namespace Service.Service;
 
 public class AccountServices : IAccountRules
 {
+    private readonly IUserRules _user;
+
+    private readonly ISessionRules _session;
+
+    private readonly IBaseRules<User> _userCrud;
+
+    public AccountServices(IUserRules user, ISessionRules session, IBaseRules<User> userCrud)
+    {
+        _user = user;
+        _session = session;
+        _userCrud = userCrud;
+    }
+
+    public async Task<ActivationStatus> ActivationAsync(ActivationViewModel activation)
+        => await Task.Run(async () =>
+       {
+           User user = await _user.GetUserByUserNameAsync(activation.UserName);
+           if (user != null)
+           {
+               if (user.ActiveCode == activation.ActiveCode)
+               {
+                   user.IsActiive = true;
+                   user.ActiveCode = 5.GenerateNumberCode();
+                   return await _userCrud.UpdateAsync(user) ?
+                        ActivationStatus.Success : ActivationStatus.Exception;
+               }
+               return ActivationStatus.WrongCode;
+           }
+           return ActivationStatus.UserNotFound;
+       });
+
     public void Dispose()
     {
-        throw new NotImplementedException();
+        GC.SuppressFinalize(this);
     }
+
+    public async Task<LoginResponse> LoginAsync(LoginViewModel login)
+        => await Task.Run(async () =>
+        {
+            User user = await _user.GetUserByUserNameAsync(login.UserName);
+            if (user != null)
+            {
+                if (user.IsActiive)
+                {
+                    Session session = await _session.CreatSessionAsync(user.Id);
+                    return session != null
+                        ? new LoginResponse(LoginStatus.Success, new(session.Key, session.Value))
+                        : new LoginResponse(LoginStatus.Exception, null);
+                }
+                return new LoginResponse(LoginStatus.UserNotActive, null);
+            }
+            return new LoginResponse(LoginStatus.UserNotFound, null);
+        });
+
+    public async Task<SignUpStatus> SignUpAsync(SignUpViewModel signUp)
+        => await Task.Run(async () =>
+        {
+            if (await _user.GetUserByUserNameAsync(signUp.UserName) == null &&
+                await _user.GetUserByUserNameAsync(signUp.Email) == null &&
+                await _user.GetUserByUserNameAsync(signUp.MobileNo) == null)
+            {
+                User newUser = await _user.CreateUserAsync(signUp);
+                return newUser != null ? SignUpStatus.Success : SignUpStatus.Exception;
+            }
+            return SignUpStatus.UserExist;
+        });
+
+
 }
