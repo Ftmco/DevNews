651c95a40788d2b8dff339be916199a822ae347c
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-16 10:58:37 -0800

    Session Service

---
+++ DevNews/Service/Rules/ISessionRules.cs
@@ -0,0 +1,13 @@
+﻿using Entity.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Rules;
+
+public interface ISessionRules : IDisposable
+{
+    Task<Session> CreatSessionAsync(Guid userId);
+}
---
+++ DevNews/Service/Service/SessionServices.cs
@@ -0,0 +1,41 @@
+﻿using Entity.User;
+using Service.Rules;
+using Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools.Crypto;
+
+namespace Service.Service;
+
+public class SessionServices : ISessionRules
+{
+    private readonly IBaseRules<Session> _sessionCrud;
+
+    public SessionServices(IBaseRules<Session> sessionCrud)
+    {
+        _sessionCrud = sessionCrud;
+    }
+
+    public async Task<Session> CreatSessionAsync(Guid userId)
+        => await Task.Run(async () =>
+        {
+            Session session = new()
+            {
+                CreateDate = DateTime.Now,
+                ExpireDate = DateTime.Now.AddDays(20),
+                Key = "Token",
+                UserId = userId,
+                Value = $"{Guid.NewGuid()}".CreateSHA256(),
+            };
+            return await _sessionCrud.InsertAsync(session) ?
+              session : null;
+        });
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
