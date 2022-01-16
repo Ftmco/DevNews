120597a32fcb7083e32a7c66d4607e97754e39b8
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-16 11:09:09 -0800

    Update Email.cs

--- DevNews/Tools/Email/Email.cs
+++ DevNews/Tools/Email/Email.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,25 @@ namespace Tools.Email;
 
 public static class EmailExtensions
 {
-    
+    public static async Task SendEmailAsync(this string email, string subject, string body)
+        => await Task.Run(async () =>
+        {
+            MailMessage mailMessage = new()
+            {
+                From = new MailAddress("article.news.tm@gmail.com"),
+                Subject = subject,
+                IsBodyHtml = true,
+                Body = body,
+            };
+            SmtpClient smtpClient = new()
+            {
+                Port = 587,
+                Host = "smtp.gmail.com",
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("article.news.tm@gmail.com", "1G14ijWA"),
+                DeliveryMethod = SmtpDeliveryMethod.Network
+            };
+            await smtpClient.SendMailAsync(mailMessage);
+        });
 }
