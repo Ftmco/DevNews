16954a34f91e960e2a9fb7c1ad59da9d22694918
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-20 07:22:43 -0800

    Create File.cs

---
+++ DevNews/Tools/File/File.cs
@@ -0,0 +1,13 @@
+﻿using static Tools.AppSetting.AppSetting;
+
+namespace Tools.FileTools;
+
+public static class FileExtensions
+{
+    public static async Task<string> CreateFileAddressAsync(this string fileName, string path)
+        => await Task.Run(async () =>
+        {
+            string? url = await "Urls".GetDataAsync("Base");
+            return $"{url}/{path}/{fileName ?? "null.png"}";
+        });
+}
