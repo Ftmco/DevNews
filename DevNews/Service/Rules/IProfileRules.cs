94c1003e8f500f43e735fda9feac4f386de68cea
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-20 07:20:12 -0800

    Create Profile Services

---
+++ DevNews/Service/Rules/IProfileRules.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel.Profile;
+
+namespace Service.Rules;
+
+public interface IProfileRules : IDisposable
+{
+    Task<GetProileResponse> GetProfileAsync(IHeaderDictionary headers);
+}
---
+++ DevNews/ViewModel/Profile/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Profile;
+
+public record ProfileViewModel(string UserName, string Email, string MobileNo, string FullName, string Image);
+
+public record GetProileResponse(ProfileStatus Status,ProfileViewModel Profile);
+
+public enum ProfileStatus
+{
+    Success = 0,
+    UserNotFound = 1,
+    Exception = 2
+}
