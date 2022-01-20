bd69401cc1d5aa261a920674527b86cc8c80868e
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-20 10:54:29 -0800

    Update Category and Data Base

---
+++ DevNews/DataLayer/Migrations/20220120165411_UpdateGroups.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    public partial class UpdateGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParrentId",
+                table: "Category",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParrentId",
+                table: "Category");
+        }
+    }
+}
---
+++ DevNews/DataLayer/Migrations/20220120181621_FileUpdate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    public partial class FileUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "Channel");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ObjectId",
+                table: "File",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ObjectId",
+                table: "File");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "Channel",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
