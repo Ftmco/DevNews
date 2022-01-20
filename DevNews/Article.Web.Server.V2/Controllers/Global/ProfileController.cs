405854f84cdd1e2f1bfaf7c159a7379aabf28569
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-20 07:21:18 -0800

    Update ProfileController.cs

--- DevNews/Article.Web.Server.V2/Controllers/Global/ProfileController.cs
+++ DevNews/Article.Web.Server.V2/Controllers/Global/ProfileController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Service.Rules;
+using ViewModel.Profile;
 
 namespace Article.Web.Server.V2.Controllers.Global;
 
@@ -6,4 +8,23 @@ namespace Article.Web.Server.V2.Controllers.Global;
 [ApiController]
 public class ProfileController : ControllerBase
 {
+    private readonly IProfileRules _profile;
+
+    public ProfileController(IProfileRules profile)
+    {
+        _profile = profile;
+    }
+
+    [HttpGet("Get")]
+    public async Task<IActionResult> Get()
+    {
+        GetProileResponse? profile = await _profile.GetProfileAsync(Request.Headers);
+        return profile.Status switch
+        {
+            ProfileStatus.Success => Ok(Success("", "User Profile", profile.Profile)),
+            ProfileStatus.UserNotFound => Ok(Faild(403, "User Not Found", "Please Login To Your Account")),
+            ProfileStatus.Exception => Ok(ApiException("Exception", "")),
+            _ => Ok(ApiException("Exception", "")),
+        };
+    }
 }
