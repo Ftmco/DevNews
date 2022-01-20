60f1bc29405be2b36ab5d82201489a5e27e62dee
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-20 10:54:09 -0800

    Update ChannelController.cs

--- DevNews/Article.Web.Server.V2/Controllers/Client/ChannelController.cs
+++ DevNews/Article.Web.Server.V2/Controllers/Client/ChannelController.cs
@@ -22,10 +22,31 @@ public class ChannelController : ControllerBase
         GetChannelsResponse? channels = await _channel.GetChannelsAsync(HttpContext);
         return channels.Status switch
         {
-            GetChannelsStatus.Success => Ok(Success("", "User Channels", channels.Channels)),
-            GetChannelsStatus.UserNotFound => Ok(Faild(403, "User Not Found", "Please Login To Your Account")),
-            GetChannelsStatus.Exception => Ok(ApiException("Exception", "")),
+            ChannelsStatus.Success => Ok(Success("", "User Channels", channels.Channels)),
+            ChannelsStatus.UserNotFound => Ok(Faild(403, "User Not Found", "Please Login To Your Account")),
+            ChannelsStatus.Exception => Ok(ApiException("Exception", "")),
             _ => Ok(ApiException("Exception", "")),
         };
+    }
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> Create(UpsertChannelViewModel insert)
+    {
+        UpsertChannelResponse? create = await _channel.CreateAsync(insert, Request.Headers);
+        return create.Status switch
+        {
+            ChannelsStatus.Success => Ok(Success("Channel Created Successfully", "", create.Channel)),
+            ChannelsStatus.UserNotFound => Ok(Faild(403, "User Not Found", "Please Login To Your Account")),
+            ChannelsStatus.Exception => Ok(ApiException("Exception", "")),
+            _ => Ok(ApiException("Exception", "")),
+        };
+    }
+
+    [HttpGet("CheckLink")]
+    public async Task<IActionResult> CheckLink(string link)
+    {
+        return await _channel.CheckLinkAsync(link) ?
+             Ok(Success("Channel Link Is Exist Please Chose An Other Link", "", new { })) :
+                Ok(Success("Use Can Use This Link", "", new { }));
     }
 }
