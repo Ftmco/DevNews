27323703b55db57da7a26593fbdadedfba2793a2
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-20 10:53:18 -0800

    Create File.cs

---
+++ DevNews/ViewModel/File/File.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.File;
+
+public record UploadFileViewModel(string Base64, string Type, string OgName);
+
+public record SaveFileViewModel(string Base64, string Path);
+
+public record SaveFileResponse(SaveFileStatus Status, string Name, string Type, int Size);
+
+public enum SaveFileStatus
+{
+    Success,
+    Exception
+}
+
+public enum FileTypes
+{
+    ChannelAvatar = 0,
+    UserProfile = 1
+}
+
+public enum ChannelAvatarType
+{
+    Main = 0,
+    Othrer = 1
+}
