8d70594b8bd81f28c66524c60e52b908b41c6e99
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-20 07:20:53 -0800

    Create Extensions Services

---
+++ DevNews/Service/Extensions/User.cs
@@ -0,0 +1,18 @@
+﻿using Entity.User;
+using Tools.FileTools;
+using ViewModel.Profile;
+
+namespace Service.Extensions;
+
+public static class UserExtensions
+{
+    public static async Task<ProfileViewModel> CreateProfileViewModelAsync(this User user)
+        => await Task.Run(async () =>
+        new ProfileViewModel(
+            UserName: user.UserName,
+            Email: user.Email,
+            MobileNo: user.MobileNo,
+            FullName: user.FullName,
+            Image: await user.Image.CreateFileAddressAsync("Files/UserProfile")
+            ));
+}
