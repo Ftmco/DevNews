277dfe6443e8af097c7c7c99666cd07cab5f8fcf
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-22 10:37:17 -0800

    channel api

--- DevNews/Article.Web.Server.V2/Controllers/Client/ChannelController.cs
+++ DevNews/Article.Web.Server.V2/Controllers/Client/ChannelController.cs
@@ -29,6 +29,19 @@ public class ChannelController : ControllerBase
         };
     }
 
+    [HttpGet("AdminChannels")]
+    public async Task<IActionResult> GetAdminChannels()
+    {
+        GetChannelsResponse? channels = await _channel.GetAdminChannelsAsync(HttpContext);
+        return channels.Status switch
+        {
+            ChannelsStatus.Success => Ok(Success("", "User Channels", channels.Channels)),
+            ChannelsStatus.UserNotFound => Ok(Faild(403, "User Not Found", "Please Login To Your Account")),
+            ChannelsStatus.Exception => Ok(ApiException("Exception", "")),
+            _ => Ok(ApiException("Exception", "")),
+        };
+    }
+
     [HttpPost("Create")]
     public async Task<IActionResult> Create(UpsertChannelViewModel insert)
     {
@@ -48,5 +61,29 @@ public class ChannelController : ControllerBase
         return await _channel.CheckLinkAsync(link) ?
              Ok(Success("Channel Link Is Exist Please Chose An Other Link", "", new { })) :
                 Ok(Success("Use Can Use This Link", "", new { }));
+    }
+
+    [HttpGet("Subscribe")]
+    public async Task<IActionResult> Subscribe(string token)
+        => await _channel.SubscribeChannelAsync(token, Request.Headers) switch
+        {
+            ChannelsStatus.Success => Ok(Success("", "Join Channel Successfully", new { })),
+            ChannelsStatus.UserNotFound => Ok(Faild(403, "User Not Found Please Login to your Account", "")),
+            ChannelsStatus.Exception => Ok(ApiException("Exception,Please Try Again", "")),
+            ChannelsStatus.ChannelNotFound => Ok(Faild(404, "Channel Is Not Defined", "")),
+            _ => Ok(ApiException("Exception,Please Try Again", "")),
+        };
+
+    [HttpGet("Channel")]
+    public async Task<IActionResult> Channel(string token)
+    {
+        ChannelInfoViewModel? channel = await _channel.GetChannelAsync(token, Request.Headers);
+        return channel.Status switch
+        {
+            ChannelsStatus.Success => Ok(Success("", $"Channel '{channel.Channel.Name}' Info", channel)),
+            ChannelsStatus.UserNotFound => Ok(Faild(403, "User Not Found Please Login to your Account", "")),
+            ChannelsStatus.Exception => Ok(ApiException("Exception,Please Try Again", "")),
+            _ => Ok(ApiException("Exception,Please Try Again", "")),
+        };
     }
 }
