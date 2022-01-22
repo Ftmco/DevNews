d3fd38a6beb9bfd400a8bcef94373f39eee97836
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-22 10:34:22 -0800

    Update ProfileServices.cs

--- DevNews/Service/Service/ProfileServices.cs
+++ DevNews/Service/Service/ProfileServices.cs
@@ -1,6 +1,7 @@
 ﻿using Entity.User;
 using Microsoft.AspNetCore.Http;
 using Service.Rules;
+using Services.Base;
 using ViewModel.Profile;
 
 namespace Service.Service;
@@ -9,9 +10,15 @@ public class ProfileServices : IProfileRules
 {
     private readonly IAccountRules _account;
 
-    public ProfileServices(IAccountRules account)
+    private readonly IBaseRules<User> _userCrud;
+
+    private readonly IBaseRules<Entity.Article.File> _fileCrud;
+
+    public ProfileServices(IAccountRules account, IBaseRules<User> userCrud, IBaseRules<Entity.Article.File> fileCrud)
     {
         _account = account;
+        _userCrud = userCrud;
+        _fileCrud = fileCrud;
     }
 
     public void Dispose()
@@ -19,17 +26,44 @@ public class ProfileServices : IProfileRules
         GC.SuppressFinalize(this);
     }
 
-    public async Task<GetProileResponse> GetProfileAsync(IHeaderDictionary headers)
+    public async Task<ProfileResponse> GetProfileAsync(IHeaderDictionary headers)
         => await Task.Run(async () =>
         {
             User user = await _account.GetUserBySessionAsync(headers);
             return user != null
-                ? new GetProileResponse(ProfileStatus.Success, await GetProfileViewModelAsync(user))
-                : new GetProileResponse(ProfileStatus.UserNotFound, null);
+                ? new ProfileResponse(ProfileStatus.Success, await GetProfileViewModelAsync(user))
+                : new ProfileResponse(ProfileStatus.UserNotFound, null);
         });
 
-    public Task<ProfileViewModel> GetProfileViewModelAsync(User user)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<ProfileViewModel> GetProfileViewModelAsync(User user)
+        => await Task.Run(async () =>
+        {
+            ProfileViewModel profile = new(
+                UserName: user.UserName,
+                Email: user.Email,
+                MobileNo: user.MobileNo,
+                FullName: user.FullName,
+                Image: await _fileCrud.GetAsync(up => up.ObjectId == user.Id));
+            return profile;
+        });
+
+    public async Task<ProfileResponse> UpdateProfileAsync(UpdateProfileViewModel profile, IHeaderDictionary headers)
+        => await Task.Run(async () =>
+        {
+            User user = await _account.GetUserBySessionAsync(headers);
+            if (user != null)
+            {
+                user.FullName = profile.FullName;
+                if (!await _userCrud.AnyAsync(u => u.UserName == profile.UserName.ToLower()))
+                {
+                    user.UserName = profile.UserName;
+                    return await _userCrud.UpdateAsync(user) ?
+                          new ProfileResponse(ProfileStatus.Success, await GetProfileViewModelAsync(user)) :
+                                new ProfileResponse(ProfileStatus.Exception, null);
+                }
+
+                return new ProfileResponse(ProfileStatus.UserNameExist, null);
+            }
+            return new ProfileResponse(ProfileStatus.UserNotFound, null);
+        });
 }
