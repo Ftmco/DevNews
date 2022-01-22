88da8a9358648c252602d683243b9946f1aa3732
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-22 10:34:09 -0800

    Update Profile.cs

--- DevNews/ViewModel/Profile/Profile.cs
+++ DevNews/ViewModel/Profile/Profile.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModel.File;
 
 namespace ViewModel.Profile;
 
-public record ProfileViewModel(string UserName, string Email, string MobileNo, string FullName, string Image);
+public record ProfileViewModel(string UserName, string Email, string MobileNo, string FullName, IEnumerable<Entity.Article.File> Image);
 
-public record GetProileResponse(ProfileStatus Status,ProfileViewModel Profile);
+public record UpdateProfileViewModel(string UserName, string FullName, UploadFileViewModel Image);
+
+public record ProfileResponse(ProfileStatus Status, ProfileViewModel Profile);
 
 public enum ProfileStatus
 {
     Success = 0,
     UserNotFound = 1,
-    Exception = 2
+    Exception = 2,
+    UserNameExist = 3
 }
