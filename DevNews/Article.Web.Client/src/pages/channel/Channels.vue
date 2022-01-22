0a1a317528d1b4a845be8a84dd0142e77dedd4ce
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-22 10:35:32 -0800

    Update Channels.vue

--- DevNews/Article.Web.Client/src/pages/channel/Channels.vue
+++ DevNews/Article.Web.Client/src/pages/channel/Channels.vue
@@ -1,29 +1,37 @@
 <template>
-  <v-card class="mx-auto" elevation="5" tile>
-    <v-list rounded>
-      <v-text-field outlined placeholder="Search" label="Search" />
-      <v-list-item-group v-model="selectedItem" color="primary">
-        <v-list-item
-          v-for="(channel, i) in channels"
-          :key="i"
-          :to="{
-            name: 'Channel',
-            query: { token: channel.token, name: channel.name },
-          }"
-        >
-          <v-list-item-avatar>
-            <v-img :src="channel.avatar" :lazy-src="channel.avatar" />
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title v-text="channel.name"></v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-avatar>
-            <v-chip color="error">5</v-chip>
-          </v-list-item-avatar>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-  </v-card>
+  <div>
+    <v-card class="mx-auto" elevation="5" tile>
+      <v-col cols="12">
+        <v-text-field outlined placeholder="Search" label="Search" />
+      </v-col>
+    </v-card>
+    <br />
+    <v-card class="max-auto" elevation="5" tile>
+      <v-list rounded>
+        <v-tabs color="deep-purple accent-4" fixed-tabs>
+          <v-tab>All Channels</v-tab>
+          <v-tab>My Channels</v-tab>
+
+          <v-tab-item>
+            <channel-list :channels="allChannels" />
+          </v-tab-item>
+          <v-tab-item>
+            <channel-list :channels="myChannels" />
+          </v-tab-item>
+        </v-tabs>
+      </v-list>
+    </v-card>
+    <v-col>
+      <v-btn elevation="5" fab color="primary" @click="newChannel">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-col>
+    <app-dialog title="Create New Channel">
+      <template slot="body">
+        <NewChannel @channelCreated="channelCreated" />
+      </template>
+    </app-dialog>
+  </div>
 </template>
 
 <script lang="ts">
@@ -32,14 +40,21 @@ import Vue from "vue";
 import ChannelService from "@/api/service/channel.service";
 import { messages } from "@/constants";
 import { showMessage } from "@/services/message";
+import ChannelList from "@/components/channel/ChannelList.vue";
+import NewChannel from "@/components/channel/NewChannel.vue";
+import AppDialog from "@/components/core/AppDialog.vue";
+
 export default Vue.extend({
+  components: { ChannelList, NewChannel, AppDialog },
   data: () => ({
     selectedItem: 1,
-    channels: [],
+    allChannels: [],
+    myChannels: [],
     channelService: new ChannelService(apiCall),
   }),
   mounted() {
     this.getChannels();
+    this.getMyChannels();
   },
   methods: {
     getChannels() {
@@ -47,12 +62,34 @@ export default Vue.extend({
       this.channelService
         .getChannels()
         .then((res) => {
-          if (res.status) this.channels = res.result;
+          if (res.status) this.allChannels = res.result;
           showMessage(this, res.title);
         })
         .catch((e) => {
           showMessage(this, messages.netWorkError(e.message).title);
         });
+    },
+    getMyChannels() {
+      (this.$root.$refs.loading as any).open();
+      this.channelService
+        .getMyChannels()
+        .then((res) => {
+          if (res.status) {
+            this.myChannels = res.result;            
+          }
+          showMessage(this, res.title);
+        })
+        .catch((e) => {
+          showMessage(this, messages.netWorkError(e.message).title);
+        });
+    },
+    newChannel() {
+      (this.$root.$refs.dialog as any).open();
+    },
+    channelCreated(channel: any) {
+      (this.$root.$refs.dialog as any).close();
+      this.myChannels.push(channel as never);
+      this.allChannels.push(channel as never);
     },
   },
 });
