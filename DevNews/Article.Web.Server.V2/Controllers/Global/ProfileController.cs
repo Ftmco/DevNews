10eaccc1996018b7ae0f772c588fe9d133ff5008
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-22 10:37:20 -0800

    Update ProfileController.cs

--- DevNews/Article.Web.Server.V2/Controllers/Global/ProfileController.cs
+++ DevNews/Article.Web.Server.V2/Controllers/Global/ProfileController.cs
@@ -18,13 +18,27 @@ public class ProfileController : ControllerBase
     [HttpGet("Get")]
     public async Task<IActionResult> Get()
     {
-        GetProileResponse? profile = await _profile.GetProfileAsync(Request.Headers);
+        ProfileResponse? profile = await _profile.GetProfileAsync(Request.Headers);
         return profile.Status switch
         {
             ProfileStatus.Success => Ok(Success("", "User Profile", profile.Profile)),
             ProfileStatus.UserNotFound => Ok(Faild(403, "User Not Found", "Please Login To Your Account")),
             ProfileStatus.Exception => Ok(ApiException("Exception", "")),
             _ => Ok(ApiException("Exception", "")),
+        };
+    }
+
+    [HttpPost("Update")]
+    public async Task<IActionResult> Update(UpdateProfileViewModel profile)
+    {
+        ProfileResponse? update = await _profile.UpdateProfileAsync(profile, Request.Headers);
+        return update.Status switch
+        {
+            ProfileStatus.Success => throw new NotImplementedException(),
+            ProfileStatus.UserNotFound => throw new NotImplementedException(),
+            ProfileStatus.Exception => throw new NotImplementedException(),
+            ProfileStatus.UserNameExist => throw new NotImplementedException(),
+            _ => throw new NotImplementedException(),
         };
     }
 }
