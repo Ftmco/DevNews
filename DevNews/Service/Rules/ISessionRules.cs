3275d4fe5f47a6b49bcda82d1ebfa5483b2840b9
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-22 11:51:50 -0800

    Update ISessionRules.cs

--- DevNews/Service/Rules/ISessionRules.cs
+++ DevNews/Service/Rules/ISessionRules.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModel.Account;
 
 namespace Service.Rules;
 
 public interface ISessionRules : IDisposable
 {
-    Task<Session> CreatSessionAsync(Guid userId);
+    Task<Session> CreatSessionAsync(Guid userId,LoginViewModel login);
 
     Task<Session> GetSessionAsync(string value);
 }
