a3a68c535d20e3417b8a30c2a04b9e0d50d2f8a4
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-22 11:51:39 -0800

    Update Login.cs

--- DevNews/ViewModel/Account/Login.cs
+++ DevNews/ViewModel/Account/Login.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace ViewModel.Account;
 
-public record LoginViewModel(string UserName,string Password);
+public record LoginViewModel(string UserName, string Password, string Platform, string UserClient);
 
-public record LoginResponse(LoginStatus Status,SessionViewModel Session);
+public record LoginResponse(LoginStatus Status, SessionViewModel Session);
 
-public record SessionViewModel(string Key,string Value);
+public record SessionViewModel(string Key, string Value);
 
 public enum LoginStatus
 {
