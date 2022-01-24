e9a0599c17f452749f670d4de48a12af58db4918
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-24 11:28:13 -0800

    update native app

--- DevNews/Article.Ionic/article/src/views/TabsPage.vue
+++ DevNews/Article.Ionic/article/src/views/TabsPage.vue
@@ -3,19 +3,24 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+        <ion-tab-button tab="Home" href="/tabs/tab1">
+          <ion-icon :icon="home" />
+          <ion-label>Home</ion-label>
         </ion-tab-button>
-          
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+
+        <ion-tab-button tab="Channels" href="/tabs/tab2">
+          <ion-icon :icon="chatbubbles"></ion-icon>
+          <ion-label>Channels</ion-label>
         </ion-tab-button>
-        
+
+        <ion-tab-button tab="Search" href="/tabs/tab2">
+          <ion-icon :icon="search"></ion-icon>
+          <ion-label>Search</ion-label>
+        </ion-tab-button>
+
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon :icon="cog"></ion-icon>
+          <ion-label>Setting</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -23,17 +28,45 @@
 </template>
 
 <script lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonRouterOutlet,
+} from "@ionic/vue";
+import {
+  ellipse,
+  square,
+  triangle,
+  home,
+  chatbubbles,
+  search,
+  cog,
+} from "ionicons/icons";
 
 export default {
-  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+  components: {
+    IonLabel,
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+    IonPage,
+    IonRouterOutlet,
+  },
   setup() {
     return {
-      ellipse, 
-      square, 
+      ellipse,
+      square,
       triangle,
-    }
-  }
-}
+      home,
+      chatbubbles,
+      search,
+      cog,
+    };
+  },
+};
 </script>
