d07634d2412e392368524984b3de23496317c026
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-28 04:48:08 -0800

    Update Toast.ts

--- DevNews/Article.Ionic/article/src/services/components/Toast.ts
+++ DevNews/Article.Ionic/article/src/services/components/Toast.ts
@@ -1,20 +1,22 @@
 import { toastController } from "@ionic/vue"
 
 export const showToast = async (message: string) => {
-    const toast = await toastController.create({
-        message: message,
-        duration: 2500,
-        color: 'dark',
-        buttons: [
-            {
-                text: 'Close',
-                role: 'cancel',
-                icon: 'close',
-                handler: () => {
-                    toast.dismiss()
+    if (message.trim() != '') {
+        const toast = await toastController.create({
+            message: message,
+            duration: 2500,
+            color: 'dark',
+            buttons: [
+                {
+                    text: 'Close',
+                    role: 'cancel',
+                    icon: 'close',
+                    handler: () => {
+                        toast.dismiss()
+                    }
                 }
-            }
-        ]
-    })
-    return toast.present()
+            ]
+        })
+        return toast.present()
+    }
 }
