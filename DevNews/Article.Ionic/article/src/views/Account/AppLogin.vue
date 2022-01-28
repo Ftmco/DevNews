d80f5f6c6bf706f33067f73d18c5b093e5399f09
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-28 04:48:12 -0800

    Update AppLogin.vue

--- DevNews/Article.Ionic/article/src/views/Account/AppLogin.vue
+++ DevNews/Article.Ionic/article/src/views/Account/AppLogin.vue
@@ -4,31 +4,106 @@
       <ion-toolbar>
         <ion-title>Login</ion-title>
       </ion-toolbar>
-      <ion-content :fullscreen="true">
-        <ion-input placeholder="User Name"></ion-input>
-      </ion-content>
     </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-card>
+        <ion-card-header>
+          <ion-card-title> Welcome to your account. </ion-card-title>
+          <img src="../../assets/images/login.png" alt="" />
+        </ion-card-header>
+        <ion-card-content>
+          <ion-item>
+            <ion-label position="floating">User Name</ion-label>
+            <ion-input
+              v-model="user.userName"
+              required
+              clear-input
+              placeholder="User Name"
+            ></ion-input>
+          </ion-item>
+
+          <ion-item>
+            <ion-label position="floating">Password</ion-label>
+            <ion-input
+            v-model="user.password"
+              type="password"
+              required
+              clear-input
+              pattern="password"
+              placeholder="Password"
+            ></ion-input>
+          </ion-item>
+
+          <ion-button expand="block" @ionFocus="login">Login</ion-button>
+        </ion-card-content>
+      </ion-card>
+    </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import {
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonContent,
+  IonInput,
+  loadingController,
+} from "@ionic/vue";
+import AccountServiec from "@/api/service/account.service";
+import { apiCall } from "@/api";
+import { showToast } from "@/services/components/Toast";
+import { messages } from "@/constants";
+import router from "@/router";
+
+export default defineComponent({
+  components: {
     IonPage,
     IonHeader,
     IonTitle,
     IonContent,
-    IonInput
-}from "@ionic/vue"
-
-
-export default defineComponent({
-    components:{
-        IonPage,
-        IonHeader,
-        IonTitle,
-        IonContent,
-        IonInput
-    }
+    IonInput,
+  },
+  data: () => ({
+    user: {
+      userName: "",
+      password: "",
+    },
+    loading: loadingController.create({
+      message: "Please Wait...",
+      spinner: "bubbles",
+    }),
+    accountServices: new AccountServiec(apiCall),
+  }),
+  methods: {
+    async login() {
+      await this.openLoading();
+      this.accountServices
+        .Login(this.user)
+        .then((res) => {
+          if (res.status) {
+            router.push({
+              name: "settings",
+            });
+          }
+          showToast(res.title);
+        })
+        .catch((e) => {
+          showToast(messages.netWorkError(e.message).message);
+        })
+        .finally(async () => {
+          await this.closeLoadin();
+        });
+    },
+    async openLoading() {
+      const loading = await this.loading;
+      loading.present();
+    },
+    async closeLoadin() {
+      const loading = await this.loading;
+      loading.dismiss();
+    },
+  },
 });
 </script>
