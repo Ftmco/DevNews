e915cb5eafff08dcce88a050f421b8bd2e33ef3a
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-30 11:09:01 -0800

    Update Core.ts

--- DevNews/Article.Ionic/article/src/services/components/Core.ts
+++ DevNews/Article.Ionic/article/src/services/components/Core.ts
@@ -1,15 +1,19 @@
 import { loadingController } from "@ionic/core"
 
 
-export const openLoading = async (message = "Please Wait...") => {
-    const loading = await loadingController.create({
-        message: message,
-        spinner: 'bubbles',
+export const openLoading = async (message = "Please Wait..."): Promise<HTMLIonLoadingElement> => {
+    return new Promise<HTMLIonLoadingElement>((resolve, reject) => {
+        loadingController.create({
+            message: message,
+            spinner: 'bubbles',
+            keyboardClose: true,
+        }).then(async (res) => {
+            await res.present()
+            resolve(res)
+        }).catch((e) => reject(e))
     })
-    loading.present()
-    return loading
 }
 
-export const closeLoading = (loading: HTMLIonLoadingElement) => {
-    loading.dismiss()
+export const closeLoading = async (loading: HTMLIonLoadingElement) => {
+    await loading.dismiss()
 }
