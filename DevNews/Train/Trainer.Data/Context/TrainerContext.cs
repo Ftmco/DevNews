051aac7b01823a6ead9b34e58b3e6faf9f8336cc
Amir Hossein <amir0hossin0pr@gmail.com>
2022-01-30 08:43:31 -0800

    Create Trainer Data Application

---
+++ DevNews/Train/Trainer.Data/Context/TrainerContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainer.Data.Context;
+
+public class TrainerContext : DbContext
+{
+    public TrainerContext(DbContextOptions<TrainerContext> options) : base(options)
+    { }
+
+    public TrainerContext()
+    { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=Trainer_Db;Integrated Security=True");
+        }
+    }
+}
