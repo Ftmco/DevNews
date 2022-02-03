43f02140278d35be802e2a2d8a114fd42c4dcf82
Amir Hossein <amir0hossin0pr@gmail.com>
2022-02-03 10:00:27 -0800

    Update Program.cs

--- DevNews/Train/Trainer/Program.cs
+++ DevNews/Train/Trainer/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using DataLayer.Context;
+using Entity.User;
+using Services.Base;
+
 Console.WriteLine("Article Social Data Trainer Is Runing Now...");
+
+IBaseRules<User> _userBase = new BaseServices<User>(new ArticleContext());
+IEnumerable<User> users = await _userBase.GetAsync();
+
