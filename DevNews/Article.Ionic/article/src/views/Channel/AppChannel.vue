22b82221d7fa49e3d24d5a1a86ac162a585cbaf2
Amir Hossein <amir0hossin0pr@gmail.com>
2022-02-03 08:21:37 -0800

    Update AppChannel.vue

--- DevNews/Article.Ionic/article/src/views/Channel/AppChannel.vue
+++ DevNews/Article.Ionic/article/src/views/Channel/AppChannel.vue
@@ -22,28 +22,44 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <post-item v-for="post in posts" :key="post.id" :post="post" />
-      <ion-infinite-scroll
-        @ionInfinite="loadData"
-        threshold="100px"
-        id="infinite-scroll"
-        :disabled="isDisabled"
-      >
-        <ion-infinite-scroll-content
-          loading-spinner="bubbles"
-          loading-text="Loading more posts..."
+      <ion-toolbar>
+        <ion-segment
+          scrollable
+          :value="page"
+          swipe-gesture
+          @ionChange="changeTab"
         >
-        </ion-infinite-scroll-content>
-      </ion-infinite-scroll>
+          <ion-segment-button value="posts">
+            <ion-label>Posts</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="articles">
+            <ion-label>Articles</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+      </ion-toolbar>
+
+      <ChannelPosts v-if="page == 'posts'" :channToken="token" />
+      <ChannelArticles v-if="page == 'articles'" />
     </ion-content>
+
     <ion-fab
       v-if="isAdmin == true"
       vertical="bottom"
-      horizontal="center"
+      horizontal="end"
       slot="fixed"
     >
-      <ion-fab-button @click="addItem">
+      <ion-fab-button @click="addItem" color="info">
         <ion-icon :icon="add"></ion-icon>
+      </ion-fab-button>
+    </ion-fab>
+    <ion-fab
+      v-if="isIn == false"
+      vertical="bottom"
+      horizontal="start"
+      slot="fixed"
+    >
+      <ion-fab-button color="info">
+        <ion-icon :icon="logIn"></ion-icon>
       </ion-fab-button>
     </ion-fab>
   </ion-page>
@@ -64,50 +80,61 @@ import {
   IonItem,
   IonIcon,
   IonButton,
-  IonInfiniteScroll,
-  IonInfiniteScrollContent,
   IonFabButton,
   IonFab,
+  IonLabel,
+  IonSegment,
+  IonSegmentButton,
 } from "@ionic/vue";
 
-import { arrowBack, add } from "ionicons/icons";
+import { arrowBack, add, logIn } from "ionicons/icons";
 import ChannelService from "@/api/service/channel.service";
 import { apiCall } from "@/api";
 import { showToast } from "@/services/components/Toast";
 import { createFileAddress } from "@/services/file";
 import CreateItems from "@/components/channel/CreateItems.vue";
 import SendFile from "@/components/channel/SendFile.vue";
-import PostItem from "@/components/channel/PostItem.vue";
+import CreateArticle from "@/components/article/CreateArticle.vue";
+import { newspaper, chatboxSharp } from "ionicons/icons";
+import ChannelPosts from "@/components/channel/ChannelPosts.vue";
+import ChannelArticles from "@/components/channel/ChannelArticles.vue";
 
 export default defineComponent({
   components: {
+    ChannelPosts,
+    ChannelArticles,
     IonPage,
     IonHeader,
     IonToolbar,
     IonTitle,
     IonContent,
     IonButtons,
-    PostItem,
     IonImg,
     IonAvatar,
     IonItem,
     IonIcon,
     IonButton,
-    IonInfiniteScroll,
-    IonInfiniteScrollContent,
     IonFabButton,
     IonFab,
+    IonLabel,
+    IonSegment,
+    IonSegmentButton,
   },
   data: () => ({
     channelName: "",
     token: "",
     channel: {},
     isAdmin: false,
+    isIn: false,
     arrowBack,
     add,
+    logIn,
     channelServices: new ChannelService(apiCall),
     posts: [],
     isDisabled: false,
+    newspaper,
+    chatboxSharp,
+    page: "posts",
   }),
   created() {
     this.channelName = this.$route.query.name?.toString() as string;
@@ -123,26 +150,23 @@ export default defineComponent({
             avatar: createFileAddress(res.result.channel.avatar[0]),
           };
           this.isAdmin = res.result.isAdmin;
-          this.getPosts(this.token);
+          this.isIn = res.result.isIn;
         }
         showToast(res.title);
       });
     },
-    getPosts(token: string) {
-      this.channelServices.getChannelPosts(token).then((res) => {
-        if (res.status) {
-          this.posts = res.result;
-        }
-        showToast(res.title);
-      });
+    changeTab(e: any) {
+      const value = e.detail.value;
+      this.page = value;
     },
     async addItem() {
       const sheetModal = await modalController.create({
-        breakpoints: [0.1, 0.5],
+        breakpoints: [0.1, 0.3, 0.5],
         backdropDismiss: true,
         swipeToClose: true,
-        initialBreakpoint: 0.3,
+        initialBreakpoint: 0.5,
         component: CreateItems,
+        mode: "ios",
       });
       sheetModal.present();
 
@@ -152,65 +176,67 @@ export default defineComponent({
     },
     async sendItem(e: any) {
       const data = e.data;
-      let component = SendFile;
-      let props = {};
-      switch (data.type) {
-        case "file": {
-          component = SendFile;
-          props = {
-            mime: "/*",
-            mimeName: "File",
-            channelToken: this.token,
-          };
-          break;
+      if (data != null) {
+        let component: any = SendFile;
+        let props = {};
+        switch (data.type) {
+          case "file": {
+            component = SendFile;
+            props = {
+              mime: "/*",
+              mimeName: "File",
+              channelToken: this.token,
+            };
+            break;
+          }
+          case "image": {
+            component = SendFile;
+            props = {
+              mime: "image/*",
+              mimeName: "Image",
+              channelToken: this.token,
+            };
+            break;
+          }
+          case "video": {
+            component = SendFile;
+            props = {
+              mime: "video/*",
+              mimeName: "Video",
+              channelToken: this.token,
+            };
+            break;
+          }
+          case "article": {
+            component = CreateArticle;
+            props = {
+              mime: "Article",
+            };
+            break;
+          }
+          case "post": {
+            component = SendFile;
+            props = {
+              mime: "post",
+              mimeName: "Post",
+              channelToken: this.token,
+            };
+            break;
+          }
         }
-        case "image": {
-          component = SendFile;
-          props = {
-            mime: "image/*",
-            mimeName: "Image",
-            channelToken: this.token,
-          };
-          break;
-        }
-        case "video": {
-          component = SendFile;
-          props = {
-            mime: "video/*",
-            mimeName: "Video",
-            channelToken: this.token,
-          };
-          break;
-        }
-        case "article": {
-          component = SendFile;
-          props = {
-            mime: "Image",
-          };
-          break;
-        }
-        case "post": {
-          component = SendFile;
-          props = {
-            mime: "post",
-            mimeName: "Post",
-            channelToken: this.token,
-          };
-          break;
-        }
+        const sendItemModal = await modalController.create({
+          breakpoints: [0.1, 0.5, 1],
+          initialBreakpoint: 0.8,
+          backdropDismiss: true,
+          swipeToClose: true,
+          component: component,
+          componentProps: props,
+        });
+        sendItemModal.present();
+        sendItemModal.onDidDismiss().then((res) => {
+          this.postSent(res);
+        });
       }
-      const sendItemModal = await modalController.create({
-        breakpoints: [0.1, 0.5, 1],
-        initialBreakpoint: 0.8,
-        backdropDismiss: true,
-        swipeToClose: true,
-        component: component,
-        componentProps: props,
-      });
-      sendItemModal.present();
-      sendItemModal.onDidDismiss().then((res) => {
-        this.postSent(res);
-      });
     },
     postSent(e: any) {
       const data = e.data;
