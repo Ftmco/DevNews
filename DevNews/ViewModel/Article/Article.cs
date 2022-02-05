c405e03fa3eb4c26e7a4a1a8df3a5e644094324b
Amir Hossein <amir0hossin0pr@gmail.com>
2022-02-05 10:55:26 -0800

    create article view models

---
+++ DevNews/ViewModel/Article/Article.cs
@@ -0,0 +1,26 @@
+﻿using ViewModel.Category;
+using ViewModel.File;
+
+namespace ViewModel.Article;
+
+public record UpsertArticleViewModel(Guid? Id, string Title, string Text, string ShortDescription,
+                UploadFileViewModel Image, IEnumerable<UploadFileViewModel> AttechmentFiles,
+                IEnumerable<Guid> Categories, string Tags, ArticleOwner Owner, string OwnerToken);
+
+public record ArticleViewModel(Guid Id, string Title, string ShortDescription, string Text, string Tags, object Owner, IEnumerable<CategoryViewModel> Categories);
+
+public record UpsertArticleResponse(UpsertArticleStatus Status, ArticleViewModel Article);
+
+public enum ArticleOwner
+{
+    Channel = 0,
+    Page = 1
+}
+
+public enum UpsertArticleStatus
+{
+    Success = 0,
+    UserNotFound = 1,
+    Exception = 2,
+    OwnerNotFound = 3
+}
