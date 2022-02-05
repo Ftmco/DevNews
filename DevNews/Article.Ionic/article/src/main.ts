513e4eab84909b56cd7931775684588173bc3c0c
Amir Hossein <amir0hossin0pr@gmail.com>
2022-02-05 10:53:22 -0800

    add quill editor

--- DevNews/Article.Ionic/article/src/main.ts
+++ DevNews/Article.Ionic/article/src/main.ts
@@ -23,10 +23,15 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
-  
+
+app.component('QuillEditor', QuillEditor)
+
 router.isReady().then(() => {
   app.mount('#app');
 });
