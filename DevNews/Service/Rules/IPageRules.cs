0fa5bef870fddeaf9a942fa399e01598e5e4224b
Amir Hossein <amir0hossin0pr@gmail.com>
2022-02-05 10:54:00 -0800

    create page services

---
+++ DevNews/Service/Rules/IPageRules.cs
@@ -0,0 +1,13 @@
+﻿using Entity.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Rules;
+
+public interface IPageRules : IDisposable
+{
+    Task<Page> GetPageByTokenAsync(string token);
+}
---
+++ DevNews/Service/Service/PageServices.cs
@@ -0,0 +1,22 @@
+﻿using Entity.Page;
+using Service.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service;
+
+public class PageServices : IPageRules
+{
+    public void Dispose()
+    {
+       GC.SuppressFinalize(this);
+    }
+
+    public Task<Page> GetPageByTokenAsync(string token)
+    {
+        throw new NotImplementedException();
+    }
+}
