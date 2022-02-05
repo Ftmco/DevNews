8092d4b07dc36f1ad0ebf2053cf27a89d91fffde
Amir Hossein <amir0hossin0pr@gmail.com>
2022-02-05 10:56:00 -0800

    create upsert api

--- DevNews/Article.Web.Server.V2/Controllers/Client/ArticleController.cs
+++ DevNews/Article.Web.Server.V2/Controllers/Client/ArticleController.cs
@@ -1,7 +1,29 @@
-﻿namespace Article.Web.Server.V2.Controllers.Client;
+﻿using ViewModel.Article;
+
+namespace Article.Web.Server.V2.Controllers.Client;
 
 [Route("api/[controller]")]
 [ApiController]
 public class ArticleController : ControllerBase
 {
+    private readonly IArticleRules _article;
+
+    public ArticleController(IArticleRules article)
+    {
+        _article = article;
+    }
+
+    [HttpPost("UpsertArticle")]
+    public async Task<IActionResult> UpsertArticle(UpsertArticleViewModel upsert)
+    {
+        UpsertArticleResponse? upsertArticle = await _article.UpdateAsync(upsert);
+        return upsertArticle.Status switch
+        {
+            UpsertArticleStatus.Success => Ok(Success("Article Created Successfully", "", upsertArticle.Article)),
+            UpsertArticleStatus.UserNotFound => Ok(Faild(403, "Please Login To your account", "")),
+            UpsertArticleStatus.Exception => Ok(ApiException("Please Try Again", "")),
+            UpsertArticleStatus.OwnerNotFound => Ok(Faild(404, "Owner Not Found...", "")),
+            _ => Ok(ApiException("Please Try Again", "")),
+        };
+    }
 }
