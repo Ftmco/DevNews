0b527b7a11bf69c310e3cee7a8d496ce26c770f4
Amir Hossein <amir0hossin0pr@gmail.com>
2022-02-23 09:20:03 -0800

    create application controller

---
+++ DevNews/Article.Web.Server.V2/Controllers/Client/ApplicationController.cs
@@ -0,0 +1,25 @@
+﻿using Entity.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Article.Web.Server.V2.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApplicationController : ControllerBase
+{
+    readonly IApplicationRules _application;
+
+    public ApplicationController(IApplicationRules application)
+    {
+        _application = application;
+    }
+
+    [HttpGet("GetLanguages")]
+    public async Task<IActionResult> GetLanguages()
+    {
+        IEnumerable<Language>? languages = await _application.GetLanguagesAsync();
+        return Ok(Success("", "", languages));
+    }
+}
+
